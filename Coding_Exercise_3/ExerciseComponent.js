0699a1b979597497bcb9fe08c7d0affd3d6adfbe
Himanshu Khobragade <45400227+lokmitra56@users.noreply.github.com>
2023-05-04 16:19:05 -0700

    Building a First Component

---
+++ Coding_Exercise_3/ExerciseComponent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// Todo: Add your component code
+// Important: Use export default function MyCmp() { ... }, don't define "export default" on a separate line!
+
+export default function ExerciseComponent () {
+    return <h2>First exercise - done!</h2>
+}
